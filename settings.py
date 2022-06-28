301f307f95218daa54d731ee3e9cc7f9e89ef38f
ohadchaet <ohad.chaet@lynx.md>
2022-06-28 19:41:44 +0300

    Refactor of showcase code

--- settings.py
+++ settings.py
@@ -1,14 +1,34 @@
 import os
+import logging
 
 AWS_REGION = os.environ["AWS_REGION"]
 AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
 AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
 
 BUCKET_NAME = os.environ["BUCKET_NAME"]
-BUCKET_DIR = os.environ["BUCKET_DIR"]
+
+METASTORE_DB_NAME = os.environ["METASTORE_DB_NAME"]
 
 HIVE_HOST = os.environ["HIVE_HOST"]
 HIVE_PORT = os.environ["HIVE_PORT"]
 
-LOCALSTACK_HOST = os.environ.get("LOCALSTACK_HOST", "localhost")
-LOCALSTACK_URL = f"http://{LOCALSTACK_HOST}:4566"
+LOCALSTACK_HOST = os.environ["LOCALSTACK_HOST"]
+LOCALSTACK_PORT = os.environ["LOCALSTACK_PORT"]
+LOCALSTACK_URL = f"http://{LOCALSTACK_HOST}:{LOCALSTACK_PORT}"
+
+# Configure the logger
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+)
+
+logging.info(f"AWS_REGION: {AWS_REGION}")
+logging.info(f"BUCKET_NAME: {BUCKET_NAME}")
+logging.info(f"METASTORE_DB_NAME: {METASTORE_DB_NAME}")
+logging.info(f"HIVE_HOST: {HIVE_HOST}")
+logging.info(f"HIVE_PORT: {HIVE_PORT}")
+logging.info(f"LOCALSTACK_HOST: {LOCALSTACK_HOST}")
+logging.info(f"LOCALSTACK_PORT: {LOCALSTACK_PORT}")
+logging.info(f"LOCALSTACK_URL: {LOCALSTACK_URL}")
+
+
